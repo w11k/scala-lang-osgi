397536594336a94aa4ca7e4979dbc154286d314a
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-05-31 20:20:44 +0200

    re #1: Fixed an initialization issue in SBT project definition; still need to check whether the created bundle is OK.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -6,10 +6,10 @@ class ScalaLangOsgiParentProject(info: ProjectInfo) extends ParentProject(info) 
   // Subprojects
   lazy val scalaLibraryProject = project("scala-library",  "scala-library",  new ScalaLibraryProject(_))
   class ScalaLibraryProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
-    override val bndBundleSymbolicName = "com.weiglewilczek.scala-lang-osgi.scala-library"
-    override val bndExportPackage = Set("scala.*;version=%s".format(buildScalaVersion))
-    override val bndImportPackage = Set("sun.*;resolution:=optional", "*")
-    override val bndFileName = "%s_%s.jar".format(project.name, buildScalaVersion)
-    override val bndClasspath = Path fromFile buildScalaInstance.libraryJar
+    override lazy val bndBundleSymbolicName = "com.weiglewilczek.scala-lang-osgi.scala-library"
+    override lazy val bndExportPackage = Set("scala.*;version=%s".format(buildScalaVersion))
+    override lazy val bndImportPackage = Set("sun.*;resolution:=optional", "*")
+    override lazy val bndFileName = "%s_%s.jar".format(project.name, buildScalaVersion)
+    override lazy val bndClasspath = Path fromFile buildScalaInstance.libraryJar
   }
 }
