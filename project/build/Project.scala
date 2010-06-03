91752aaaa5fd8b8c18578f21c4ccfa89fc9e1388
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-06-03 08:30:41 +0200

    re #1: Trying to use ManagedStyle.Maven in order to be able to deploy to Scala-Tools repo. Yet publish yields errors!

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,13 +1,20 @@
 import com.weiglewilczek.bnd4sbt.BNDPlugin
 import com.weiglewilczek.bnd4sbt.ExecutionEnvironments._
 import sbt.{ DefaultProject, ParentProject, ProjectInfo }
+import sbt.ManagedStyle._
 import sbt.Path._
+import sbt.Resolver._
 
 /** scala-lang-osgi parent project */
 class ScalaLangOsgiParentProject(info: ProjectInfo) extends ParentProject(info) {
 
   // Subprojects
   lazy val scalaLibraryProject = project("scala-library",  "scala-library",  new ScalaLibraryProject(_))
+
+  // Publishing
+  override def managedStyle = Maven
+  lazy val testRepo  = file("test-repo", new java.io.File(fileProperty("java.io.tmpdir").absolutePath, "test-repo"))
+  lazy val publishTo = testRepo
 
   /** scala-library subproject */
   class ScalaLibraryProject(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
@@ -28,5 +35,7 @@ class ScalaLangOsgiParentProject(info: ProjectInfo) extends ParentProject(info) 
     override lazy val bndNoUses        = true
 
     override lazy val bndClasspath = fromFile(buildScalaInstance.libraryJar)
+
+    override def managedStyle = Maven
   }
 }
